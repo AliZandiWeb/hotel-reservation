e7e7939ee19b35e7a38224dc27a7bc9b09572a57
ArliyA <ali.zandi.web@gmail.com>
2024-11-13 19:50:07 +0330

    add mongodb and fix go-isatty

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
+	"log"
 
 	"github.com/AliZandiWeb/hotel-reservation/api"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+const dburi = "mongodb://localhost:27017"
+
 func main() {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(client)
+
 	listenAddr := flag.String("listenAddr", ":5000", "the listen address of the api server")
 	flag.Parse()
 
